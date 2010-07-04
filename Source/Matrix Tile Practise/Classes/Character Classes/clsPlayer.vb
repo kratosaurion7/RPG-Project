de77053369969039f52c64084565f476f4430eff
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-03 23:42:54 -0400

    The player class. Contain attributes, level, name and information on the character.

---
+++ Source/Matrix Tile Practise/Classes/Character Classes/clsPlayer.vb
@@ -0,0 +1,88 @@
+﻿Public Class clsPlayer
+    '///// Player stuff \\\\\
+    Dim pcname As String
+    Dim pclevel As Integer
+    Dim pcsprite As Image
+    '///// Background stuff \\\\\
+    Dim outputBox As MessageBar_Console.ctrMessageBar
+
+#Region "Stats"
+    Dim pcStrength
+    Dim pcDexterity
+    Dim pcConstitution
+    Dim pcIntelligence
+    Dim pcWisdom
+    Dim pcCharisma
+#End Region
+
+    Public Sub changeName(ByVal newName As String)
+        pcname = newName
+    End Sub
+
+    Public Sub levelUp()
+        pclevel += 1
+        Dim message As String = pcname + " has leveled ! You are now lv" + pclevel
+        outputBox.AddMSG(message)
+    End Sub
+
+    Public Sub changeLevel(ByVal newLevel As Integer)
+        pclevel = newLevel
+    End Sub
+
+    Public Sub New(ByRef paramOutputBox As MessageBar_Console.ctrMessageBar)
+        outputBox = paramOutputBox
+        pcsprite = My.Resources.Link
+    End Sub
+
+#Region "Properties"
+    Public Property Strength
+        Get
+            Return pcStrength
+        End Get
+        Set(ByVal value)
+            pcStrength = value
+        End Set
+    End Property
+    Public Property Dexterity
+        Get
+            Return pcDexterity
+        End Get
+        Set(ByVal value)
+            pcDexterity = value
+        End Set
+    End Property
+    Public Property Constitution
+        Get
+            Return pcConstitution
+        End Get
+        Set(ByVal value)
+            pcConstitution = value
+        End Set
+    End Property
+    Public Property Intelligence
+        Get
+            Return pcIntelligence
+        End Get
+        Set(ByVal value)
+            pcIntelligence = value
+        End Set
+    End Property
+    Public Property Wisdom
+        Get
+            Return pcWisdom
+        End Get
+        Set(ByVal value)
+            pcWisdom = value
+        End Set
+    End Property
+    Public Property Charisma
+        Get
+            Return pcCharisma
+        End Get
+        Set(ByVal value)
+            pcCharisma = value
+        End Set
+    End Property
+#End Region
+
+End Class
