37d933f1020c1201f5b08c620d9416d1de51351e
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-03 23:42:14 -0400

    This file will be the root of the application, where it starts and where it ends. From here the forms will be opened and global variables allocated.

---
+++ Source/Matrix Tile Practise/main.vb
@@ -0,0 +1,11 @@
+﻿Public Class main
+    'This is the root class of the application, from here everything is started.
+
+
+    Sub main()
+        Dim mainMenu As New frmMainMenu
+        mainMenu.Show()
+    End Sub
+
+
+End Class
