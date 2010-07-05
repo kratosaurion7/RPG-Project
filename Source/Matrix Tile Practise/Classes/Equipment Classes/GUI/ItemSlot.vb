fcec7464bb8564457a6ed8c59506afd55af5f033
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-05 01:50:28 -0400

    Changed the ItemSlot class, added a default size and a fitting variable. Still being worked on though.

--- Source/Matrix Tile Practise/Classes/Equipment Classes/GUI/ItemSlot.vb
+++ Source/Matrix Tile Practise/Classes/Equipment Classes/GUI/ItemSlot.vb
@@ -1,4 +1,13 @@
 ﻿Public Class ItemSlot
     Inherits PictureBox
 
+    Public canFit As clsItem
+
+    Public Sub New()
+        MyBase.New()
+        Me.Size = New Size(64, 64)
+
+    End Sub
+
+
 End Class
