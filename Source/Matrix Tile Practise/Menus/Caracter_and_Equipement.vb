a0cc0fa75c8972294cedd269aac9a2c5f823d83d
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-05 01:49:31 -0400

    Added the code to give the values to the labels on the form, taking it from the class in parameter.

--- Source/Matrix Tile Practise/Menus/Caracter_and_Equipement.vb
+++ Source/Matrix Tile Practise/Menus/Caracter_and_Equipement.vb
@@ -1,10 +1,26 @@
 ﻿Public Class frmCaracter_and_Equipement
 
-    Private Sub Panel1_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pnlInventory.Resize
-        Me.Text = Me.Size.ToString
-    End Sub
+    Dim characterSheet As clsPlayer
 
     Private Sub frmCaracter_and_Equipement_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'Atribute the values to their respective labels.
+        lblHeroName.Text = characterSheet.pcname
+        lblLevel.Text = characterSheet.pclevel
+        lblClass.Text = characterSheet.pcClass.className
+        lblStr.Text = characterSheet.Strength
+        lblCon.Text = characterSheet.Constitution
+        lblDex.Text = characterSheet.Dexterity
+        lblInt.Text = characterSheet.Intelligence
+        lblWis.Text = characterSheet.Wisdom
+        lblCha.Text = characterSheet.Charisma
+    End Sub
 
+    Public Sub New(ByRef sheet As clsPlayer)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        characterSheet = sheet 'Give the form the date it needs to show information on the character. Also needed to modify its equipement.
     End Sub
 End Class
