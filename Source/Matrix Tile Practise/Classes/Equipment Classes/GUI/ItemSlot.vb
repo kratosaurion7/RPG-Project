c34750974b6ede8a5c4c6a907b15fd75d83af97a
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-05 23:01:58 -0400

    Changed the canFit variable to a collection, to contain all the types that the slot can contain.

--- Source/Matrix Tile Practise/Classes/Equipment Classes/GUI/ItemSlot.vb
+++ Source/Matrix Tile Practise/Classes/Equipment Classes/GUI/ItemSlot.vb
@@ -1,7 +1,7 @@
 ﻿Public Class ItemSlot
     Inherits PictureBox
 
-    Public canFit As clsItem
+    Public canFit As Collection 'The variable that dictates what kind of item can be fitted inside the slot
 
     Public Sub New()
         MyBase.New()
