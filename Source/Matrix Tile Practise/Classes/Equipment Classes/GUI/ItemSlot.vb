fc19ca7c2bbf4edbbb3e0101646c4f2027408fd7
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-08 22:15:53 -0400

    XML Comment added

--- Source/Matrix Tile Practise/Classes/Equipment Classes/GUI/ItemSlot.vb
+++ Source/Matrix Tile Practise/Classes/Equipment Classes/GUI/ItemSlot.vb
@@ -1,4 +1,8 @@
-﻿Public Class ItemSlot
+﻿''' <summary>
+''' Class that represend a slot in which an item can be dropped
+''' </summary>
+''' <remarks></remarks>
+Public Class ItemSlot
     Inherits PictureBox
 
     Public canFit As Collection 'The variable that dictates what kind of item can be fitted inside the slot
