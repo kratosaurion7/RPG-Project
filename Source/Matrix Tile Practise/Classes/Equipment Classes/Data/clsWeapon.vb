2e853b97802b857f003cb3a9732088f4f3bfea03
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-09 21:36:50 -0400

    Added the tag assignement to inside the New()

--- Source/Matrix Tile Practise/Classes/Equipment Classes/Data/clsWeapon.vb
+++ Source/Matrix Tile Practise/Classes/Equipment Classes/Data/clsWeapon.vb
@@ -3,9 +3,9 @@
     Public minDmg As Integer
     Public maxDmg As Integer
 
-    Public Sub New(ByVal pName As String, ByVal min As Integer, ByVal max As Integer)
+    Public Sub New(ByVal pName As String, ByVal min As Integer, ByVal max As Integer, ByVal ptags() As String)
         MyBase.New(pName)
-
+        tags.Add(ptags)
     End Sub
 
     Public Function findDamage(ByVal min As Integer, ByVal max As Integer)
