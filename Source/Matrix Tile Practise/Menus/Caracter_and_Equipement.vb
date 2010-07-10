a2002d202fa60a1a389b691be51ced6c6c6389ac
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-09 21:39:14 -0400

    Changed the showItem sub, changed the key number on the assignement of the slots. They start at 1 apparently, XML comment added. And more !  Exept I don't remember and I can't scroll the comparing boxes.

--- Source/Matrix Tile Practise/Menus/Caracter_and_Equipement.vb
+++ Source/Matrix Tile Practise/Menus/Caracter_and_Equipement.vb
@@ -1,49 +1,79 @@
 ﻿Public Class frmCaracter_and_Equipement
 
-    Dim characterSheet As clsPlayer 'New ?
-    Dim inventorySlots As Collection
+    Public character As clsPlayer 'New ?
+    Public itemSheet As clsItemData
+    Public inventorySlots As New Collection 'The collection containing every slot of the inventory
 
     Private Sub frmCaracter_and_Equipement_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'Atribute the values to their respective labels.
-        lblHeroName.Text = characterSheet.pcname
-        lblLevel.Text = characterSheet.pclevel
-        lblClass.Text = characterSheet.pcClass.className
-        lblStr.Text = characterSheet.Strength
-        lblCon.Text = characterSheet.Constitution
-        lblDex.Text = characterSheet.Dexterity
-        lblInt.Text = characterSheet.Intelligence
-        lblWis.Text = characterSheet.Wisdom
-        lblCha.Text = characterSheet.Charisma
+        '///// Info Attribution \\\\\
+        lblHeroName.Text = character.pcname
+        lblLevel.Text = character.pclevel
+        lblClass.Text = character.pcClass.className
+        lblStr.Text = character.Strength
+        lblCon.Text = character.Constitution
+        lblDex.Text = character.Dexterity
+        lblInt.Text = character.Intelligence
+        lblWis.Text = character.Wisdom
+        lblCha.Text = character.Charisma
 
+        _testFillItems()
     End Sub
 
     Public Sub initItemSlots()
         'Might be able to replace those with a ForEach but only if they are placed in order, else the third itemSlot might be given the key 4.
-        inventorySlots.Add(isSlot0, 0)
-        inventorySlots.Add(isSlot1, 1)
-        inventorySlots.Add(isSlot2, 2)
-        inventorySlots.Add(isSlot3, 3)
-        inventorySlots.Add(isSlot4, 4)
-        inventorySlots.Add(isSlot5, 5)
-        inventorySlots.Add(isSlot6, 6)
-        inventorySlots.Add(isSlot7, 7)
-        inventorySlots.Add(isSlot8, 8)
-        inventorySlots.Add(isSlot9, 9)
-        inventorySlots.Add(isSlot10, 10)
-        inventorySlots.Add(isSlot11, 11)
-        inventorySlots.Add(isSlot12, 12)
-        inventorySlots.Add(isSlot13, 13)
-        inventorySlots.Add(isSlot14, 14)
-        inventorySlots.Add(isSlot15, 15)
+        inventorySlots.Add(isSlot0, 1)
+        inventorySlots.Add(isSlot1, 2)
+        inventorySlots.Add(isSlot2, 3)
+        inventorySlots.Add(isSlot3, 4)
+        inventorySlots.Add(isSlot4, 5)
+        inventorySlots.Add(isSlot5, 6)
+        inventorySlots.Add(isSlot6, 7)
+        inventorySlots.Add(isSlot7, 8)
+        inventorySlots.Add(isSlot8, 9)
+        inventorySlots.Add(isSlot9, 10)
+        inventorySlots.Add(isSlot10, 11)
+        inventorySlots.Add(isSlot11, 12)
+        inventorySlots.Add(isSlot12, 13)
+        inventorySlots.Add(isSlot13, 14)
+        inventorySlots.Add(isSlot14, 15)
+        inventorySlots.Add(isSlot15, 16)
 
     End Sub
 
-    Public Sub showItems(ByVal characterSheet As clsPlayer)
+    ''' <summary>
+    ''' Take the items in the character sheet and put them in slots inside the inventory.
+    ''' </summary>
+    ''' <param name="characterSheet">The clsPlayer instance</param>
+    ''' <remarks></remarks>
+    Public Sub showItems(ByRef characterSheet As clsPlayer)
+        Dim i As Integer = 1 'Counter
         For Each e As clsItem In characterSheet.inventory
-            Dim i As Integer
-            inventorySlots.Item(i).image = e.itemPicture
+            inventorySlots.Item(i).itemIn = e
             i += 1
         Next
+
+        updateInventory()
+    End Sub
+
+    ''' <summary>
+    ''' Refresh the image of the itemSlots in the inventory panel
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub updateInventory()
+        For Each e As ItemSlot In pnlInventory.Controls
+            If e.itemIn Is Nothing Then
+                'do nothing
+            Else
+                e.Image = e.itemIn.iPicture
+            End If
+        Next
+
+    End Sub
+
+    Public Sub _testFillItems()
+        character.addItem(itemSheet.ironSword)
+        character.addItem(itemSheet.squirePlate)
+        showItems(character)
     End Sub
 
     Public Sub New(ByRef sheet As clsPlayer)
@@ -52,6 +82,7 @@
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
-        characterSheet = sheet 'Give the form the date it needs to show information on the character. Also needed to modify its equipement.
+        character = sheet 'Give the form the date it needs to show information on the character. Also needed to modify its equipement.
+        initItemSlots() 'Put the item slots inside the inventory slots collection.
     End Sub
 End Class
