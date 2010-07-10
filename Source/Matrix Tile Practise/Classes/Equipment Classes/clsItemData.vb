bc88fc6a19047e2649027d91b0ef4f4f349b793a
Tristan Leroux <kratos_aurion7@hotmail.com>
2010-07-09 21:42:25 -0400

    Created the itemData class who holds the instances of every item in the game.

---
+++ Source/Matrix Tile Practise/Classes/Equipment Classes/clsItemData.vb
@@ -0,0 +1,26 @@
+﻿Public Class clsItemData
+
+    Public ironSword As New clsWeapon("Iron Sword", 5, 10, {"Iron", "Sword", "Weapon"})
+    Public squirePlate As New clsArmor("Plate of the Squire", {"Squire", "Plate", "Armor"})
+
+    'Need to get the relative path for this one. At least get the path to the working directory and add from there.
+    'Public directory as String = My.Application.Info.DirectoryPath
+    Public directory As String = "C:\Users\Leroux\Documents\Visual Studio 2010\Projects\TFS_Root\Production\RPG_Project\Alpha Builds\RPG-Project.git\Source\Matrix Tile Practise\Textures\Items"
+
+    Public Sub init()
+        ironSword.description = "That is an iron sword"
+        ironSword.iPicture = Image.FromFile(directory & "\sword_iron.png")
+
+        squirePlate.description = "This is a plate armor of a squire"
+        squirePlate.iPicture = Image.FromFile(directory & "\plate_squire.png")
+
+
+
+    End Sub
+
+    Public Sub New()
+        init()
+    End Sub
+
+
+End Class
